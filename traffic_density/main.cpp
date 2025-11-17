f0fa7283e5e91421a2f1b3adef355ebe6b947e67
Marcelo Vega <mgsvega@hotmail.com>
2025-11-17 02:58:51 -0300

    started refactor to make code useable

--- traffic_density/main.cpp
+++ traffic_density/main.cpp
@@ -1,16 +1,71 @@
+#include <iostream>
 #include <string>
+#include <algorithm>
 
+// ----------------------------------------------------
+// DEMO version of image_capture
+// ----------------------------------------------------
+std::pair<std::string, std::string> image_capture_demo() {
+    std::cout << "[DEMO] Simulating image capture...\n";
+    // Fake demo image data generation here
+}
+
+// ----------------------------------------------------
+// LIVE version of image_capture
+// ----------------------------------------------------
+std::pair<std::string, std::string> image_capture_live() {
+    std::cout << "[LIVE] Capturing real image from camera...\n";
+    // Actual camera capture code here
+    return ingest_camera()
+}
+
+// ----------------------------------------------------
 // Forward declaration (to be implemented in traffic_density.cpp)
 void analyzeTrafficDensity(const std::string& imagePath, const std::string& avenueName);
 
-int main() {
-	auto [avenueName, imagePath] = ingest_camera();
-    auto [report] = analyzeTrafficDensity(imagePath, avenueName);
+// ----------------------------------------------------
+int main(int argc, char* argv[]) {
 
-    sendTrafficNotification(
-    avenueName,
-    report,
-    );
+    std::string mode;
+    if (argc > 1) mode = argv[1];
+    else {
+        std::cout << "Select mode (demo/live): ";
+        std::cin >> mode;
+    }
 
-	return 0;
+    // Convert to lowercase
+    for (auto &c : mode) c = std::tolower(c);
+
+    if (mode == "demo") {
+        while (true) {
+            auto [avenueName, imagePath] = image_capture_demo();
+            auto [report] = analyzeTrafficDensity();
+            sendTrafficNotification(
+                avenueName,
+                report,
+            );
+
+            std::this_thread::sleep_for(std::chrono::seconds(5));
+        }
+    }
+
+    else if (mode == "live") {
+        while (true) {
+            auto [avenueName, imagePath] = image_capture_live();
+            auto [report] = analyzeTrafficDensity();
+            sendTrafficNotification(
+                avenueName,
+                report,
+            );
+
+            std::this_thread::sleep_for(std::chrono::seconds(36));
+        }
+    }
+    else {
+        std::cout << "Invalid mode.\n";
+        return 1;
+    }
+
+    return 0;
 }
+
